7fdec0ea77ee14bd49e504d857f9a2b4450dbcba
KineticJetIce245 <kineticjetice245@gmail.com>
2026-01-07 12:12:08 -0500

    fix pwsh autocomplete trailing backslash problem

--- rc.bat
+++ rc.bat
@@ -1,4 +1,25 @@
 @echo off
-set PYTHONPATH=%~dp0/src
+setlocal
+
 @rem passing the script location, script directory, directory the script is run from, and any additional arguments to the Python script
-python "%~dp0main.py" "%~dp0." "%CD%" %*
+set "PYTHONPATH=%~dp0/src"
+set "SCRIPT_DIR=%~dp0"
+set "CUR_DIR=%CD%"
+set "ARGS="
+
+:loopstart
+if "%~1"=="" goto loopend
+
+@rem fix the trailing backslash due to powershell autocomplete
+set "ARG=%~1"
+if "%ARG:~-1%"=="\" set "ARG=%ARG%."
+
+@rem Add to ARGS with quotes
+set "ARGS=%ARGS% "%ARG%""
+
+shift
+goto loopstart
+
+:loopend
+py "%SCRIPT_DIR%main.py" "%SCRIPT_DIR%." "%CUR_DIR%" %ARGS%
+endlocal
